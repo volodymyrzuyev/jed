9dc4dc99f3d2bbe2192609d57829ee24fa5b469b
Volodymyr Zuyev <volodymyrzuyev@gmail.com>
2025-02-17 13:07:24 -0600

    moved stuff around

---
+++ src/main/java/com/it355/jed/Employee.java
@@ -0,0 +1,31 @@
+package com.it355.jed;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String adress;
+    private String department;
+    private String wage;
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getWage() {
+        return wage;
+    }
+
+}
+
---
+++ src/main/java/com/it355/jed/Utils.java
@@ -0,0 +1,5 @@
+package com.it355.jed;
+
+public class Utils {
+    
+}
