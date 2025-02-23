35caff242b2439887470b31eb458e6576a3cde54
Volodymyr Zuyev <volodymyrzuyev@gmail.com>
2025-02-22 21:51:36 -0600

    Employee done? lmk on teams

---
+++ src/main/java/com/it355/jed/DepartmentList.java
@@ -0,0 +1,67 @@
+package com.it355.jed;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DepartmentList {
+    private HashMap<String, EmployeeList> departments;
+    private ArrayList<Employee> allEmps;
+
+    DepartmentList() {
+        this.departments = new HashMap<String, EmployeeList>();
+        this.allEmps = new ArrayList<Employee>();
+    }
+
+    public void addEmployee(String firstName, String lastName, int id, String address, String department, Double wage) {
+        Employee emp = new Employee(firstName, lastName, id, address, department, wage);
+        addEmployee(emp);
+    }
+
+    public void addEmployee(Employee emp) {
+        EmployeeList empList = getEmployeeList(emp.getDepartment());
+        addEmp(emp, empList);
+    }
+
+    public Employee findEmployee(int empID) {
+        for (int i = 0; i < allEmps.size(); i++) {
+            if (allEmps.get(i).getID() == empID) {
+                return allEmps.get(i);
+            }
+        }
+        return null;
+    }
+
+    public String printTree() {
+        String out = "";
+
+        for (String dept : departments.keySet()) {
+            out += dept + "\n";
+            out += departments.get(dept).getTree("    ");
+        }
+
+        return out;
+    }
+
+    public String toString() {
+        String out = "";
+
+        for (Employee emp : allEmps) {
+            out += emp.toString();
+        }
+
+        return out;
+    }
+
+    private EmployeeList getEmployeeList(String department) {
+        if (!departments.containsKey(department)) {
+            departments.put(department, new EmployeeList());
+        }
+
+        return departments.get(department);
+    }
+
+    private void addEmp(Employee emp, EmployeeList empl) {
+        allEmps.add(emp);
+        empl.addEmployee(emp);
+    }
+}
--- src/main/java/com/it355/jed/Employee.java
+++ src/main/java/com/it355/jed/Employee.java
@@ -1,18 +1,32 @@
 package com.it355.jed;
 
 public class Employee {
+
     private String firstName;
     private String lastName;
-    private String adress;
+    private int empId;
+    private String address;
     private String department;
-    private String wage;
+    private Double wage;
+    private EmployeeList managmentList;
 
-    public String getAdress() {
-        return adress;
+    Employee(String firstName, String lastName, int id, String address, String department, Double wage) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.empId = id;
+        this.address = address;
+        this.department = department;
+        this.wage = wage;
+        this.managmentList = new EmployeeList();
     }
 
-    public String getDepartment() {
-        return department;
+    Employee(Employee orig) {
+        this.firstName = orig.firstName;
+        this.lastName = orig.lastName;
+        this.address = orig.address;
+        this.department = orig.department;
+        this.wage = orig.wage;
+        this.managmentList = orig.managmentList;
     }
 
     public String getFirstName() {
@@ -23,9 +37,58 @@ public class Employee {
         return lastName;
     }
 
-    public String getWage() {
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public int getID() {
+        return this.empId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public Double getWage() {
         return wage;
     }
 
-}
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 
+    public void setWage(Double wage) {
+        this.wage = wage;
+    }
+
+    public void addEmployeeToManage(Employee employee) {
+        managmentList.addEmployee(employee);
+    }
+
+    public String managmentTree(String indent) {
+        String out = indent + getFullName() + "\n";
+        out += managmentList.getTree(indent + "    ");
+        return out;
+    }
+
+    public Double calculatePay(Double hours) {
+        Double rem = hours - 40;
+        if (rem < 0) {
+            return hours * wage;
+        } else {
+            return (40 * wage) + (rem * wage * 1.5);
+        }
+    }
+
+    public String toString() {
+        return String.format("Name: %s %s, ID: %d, Department: %s, Manages: %sLives At: %sMakes: %.2f$ per hour",
+                firstName,
+                lastName, empId, department, managmentList.getListNames(), address, wage);
+
+    }
+
+}
