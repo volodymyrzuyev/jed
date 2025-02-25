6faca2cad02c6758b3681da62b2db050411425e0
Thanvi Ambala <Tambala@ilstu.edu>
2025-02-25 16:35:15 -0600

    Input done

---
+++ src/main/java/com/it355/jed/input/Input.java
@@ -0,0 +1,19 @@
+package com.it355.jed;
+
+import java.util.*;
+
+public class Input{
+      private int id;
+      private String name;
+      private double salary;
+      private String department;
+
+      public Input(int id, String name, double salary, String department) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+      }
+
+     
+}
