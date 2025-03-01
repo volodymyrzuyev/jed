7518142ea322e7af71748095327a38f7ae0cd902
mattholliday123 <mattholliday430@sbcglobal.net>
2025-03-01 11:55:16 -0600

    Added rules folder and 2 individual rules

---
+++ src/main/java/com/it355/jed/rules/OBJ01_Main.java
@@ -0,0 +1,40 @@
+package com.it355.jed.rules;
+//Rule: OBJ01-J - Limit accessibility of fields
+//Recommendation: OBJ51-J - Minimize the accessibility of classes and their members
+//Matt Holliday
+
+//package private class
+final class OBJ01_LimitedBank {
+    //declare variable private
+    private double balance;
+
+    /**
+     * get balance
+     * @return
+     */
+     double getBalance()
+    {
+        return balance;
+    }
+    /**
+     * set balance
+     * @param balance
+     */
+     void setBalance(double balance)
+    {
+        this.balance = balance;
+    }
+}
+
+//Driver class
+final class OBJ01_Main
+{
+    //main method
+    public static void main()
+    {
+        OBJ01_LimitedBank b = new OBJ01_LimitedBank();
+        b.setBalance(100);
+        System.out.println("Balance" + b.getBalance());
+        
+    }
+}
